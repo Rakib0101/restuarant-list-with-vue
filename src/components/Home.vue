fb4ede81b8a94626d4d6063b91a2dd70bd538b73
Rakib0101 <rakib.01jan@gmail.com>
2022-06-27 15:37:21 +0600

    restuarant list fetch from api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,10 +4,29 @@
     <div class="wrapper container mx-auto">
         <h2 class="text-center text-4xl mt-12">Hi {{name}} ! Welcome to our website.</h2>
     </div>
+    <table class="table-auto w-1/2 mx-auto border p-4 rounded my-4">
+        <thead  class="bg-gray-600 py-2 text-2xl text-white">
+            <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Address</td>
+                <td>Contact</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in restuarants" :key="item.id" class="border-b hover:bg-gray-200 transition-all">
+                <td>{{item.id}}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.address }}</td>
+                <td>{{ item.contact }}</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 </template>
 
 <script>
+import axios from "axios";
 import Header from './Header.vue'
 export default {
     name: "Home",
@@ -16,10 +35,11 @@ export default {
     },
     data(){
         return{
-            name:''
+            name:'',
+            restuarants: []
         }
     },
-    mounted() {
+    async mounted() {
         let user = localStorage.getItem('user-info');
         this.name = JSON.parse(user).name
         if (!user) {
@@ -27,6 +47,9 @@ export default {
                 name: 'SignUp'
             })
         }
+        let result = await axios.get("http://localhost:3000/restuarants")
+        
+        this.restuarants= result.data
     }
 }
 </script>
